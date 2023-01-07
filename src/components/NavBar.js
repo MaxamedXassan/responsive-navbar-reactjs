05eed26395dd00e856fa0f924be9903e4bdc7802
maxamed <hassanhuriye05@gmail.com>
2023-01-07 06:26:27 +0300

    commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,20 @@
 import "./NavBarStyles.css";
 
-import React from 'react'
+import React, { useState } from 'react'
 
 import { FaBars } from "react-icons/fa";
 import { FaTimes } from "react-icons/fa";
 
 const NavBar = () => {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+
+
   return (
     <div className="navbar">
        <h2>Huriye</h2>
        <div>
-       <ul id="menu">
+       <ul className={click ? "menu active" : "menu"}>
         <li><a href="index.html">Home</a></li>
         <li><a href="index.html">Project</a></li>
         <li><a href="index.html">About</a></li>
@@ -19,9 +23,11 @@ const NavBar = () => {
        </ul>
       
        </div>
-       <div className="hamburger">
-        <FaTimes size={20} style={{color : "#fff"}}/>
-        <FaBars size={20} style={{color : "#fff"}}/>
+       <div className="hamburger" onClick={ handleClick}>
+        {click ? (<FaTimes size={20} style={{color : "#fff"}}/>)
+        : ( <FaBars size={20} style={{color : "#fff"}}/>)}
+        
+       
        </div>
     </div>
   )
